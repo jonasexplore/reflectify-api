665ab78b6d174bda1d8aa2ad7f48b5b9d754be61
Jonas Brito <jonasexplore@gmail.com>
2024-04-27 18:49:59 -0300

    feat(server): create socket server

---
+++ server.ts
@@ -0,0 +1,83 @@
+import http from "http";
+import express from "express";
+import pino from "pino";
+import pinoHttp from "pino-http";
+import pretty from "pino-pretty";
+import { Server, Socket } from "socket.io";
+
+const PORT = process.env.PORT;
+
+const stream = pretty({
+  colorize: true,
+  translateTime: "SYS:h:MM:ss TT",
+  ignore: "pid,hostname",
+});
+
+export type ClientProps = {
+  id: string;
+  roomId: string;
+  sender?: string;
+};
+
+const clients = new Map<Socket, ClientProps>();
+
+const logger = pino({}, stream);
+
+const httpLogger = pinoHttp({ level: "info" }, stream);
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  path: "/api/socket",
+  addTrailingSlash: false,
+  cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+  const roomId = socket.handshake?.query?.roomId as string;
+  socket.broadcast.to(roomId).emit("welcome", socket.id);
+
+  const metadata = {
+    id: socket.id,
+    roomId,
+  };
+
+  socket.join(roomId);
+  clients.set(socket, metadata);
+
+  socket.on("connect:update_board", (payload) => {
+    if (clients.size <= 1) {
+      return;
+    }
+
+    const { welcomeId, board } = payload;
+    const client = clients.get(socket);
+
+    if (!client) {
+      return;
+    }
+
+    socket.to(welcomeId).emit("update:board_updated", JSON.stringify(board));
+  });
+
+  socket.on("update:board", (payload) => {
+    const client = clients.get(socket);
+
+    if (!client) {
+      return;
+    }
+
+    socket.to(client.roomId).emit("update:board_updated", payload);
+  });
+
+  socket.on("disconnect", async () => {
+    clients.delete(socket);
+  });
+});
+
+app.use(httpLogger);
+
+server.listen(PORT, () => {
+  logger.info(`listening on *:${PORT}`);
+});
